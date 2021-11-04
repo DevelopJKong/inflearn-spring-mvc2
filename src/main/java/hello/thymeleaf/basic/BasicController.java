bdfe21ff45fce2ac888ec84305d9c01c83f2e7c3
DevelopJKong <ybaram40@gmail.com>
2021-11-04 13:02:54 +0900

    템플릿 레이아웃 완료

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -111,6 +111,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user",new User("UserA",10));
+        addUser(model);
+
+        return "basic/javascript";
+    }
+
 
     private void addUsers(Model model) {
         ArrayList<User> list = new ArrayList<>();
